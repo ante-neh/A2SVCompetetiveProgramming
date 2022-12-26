a6e948e9945a37e6a3d69db119cc2eef4d5c4044
ante-neh <amareanteneh12@gmail.com>
2022-12-26 19:03:43 +0300

    python track

---
+++ DefaultDict Tutorial/solution.py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import defaultdict
+n, m = map(int, input().split())
+d = defaultdict(list)
+
+for i in range(1, n + 1):
+    d[input()].append(str(i))
+    
+for i in range(m):
+    b = input()
+    
+    if b in d:
+        print(' '.join(d[b]))
+    
+    else:
+        print(-1)
