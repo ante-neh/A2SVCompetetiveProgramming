9bf3be4bbf63b779eeb8ada8a77caf02ce23d934
ante-neh <amareanteneh12@gmail.com>
2022-12-28 19:20:00 +0300

    problem statement

---
+++ Power - Mod Power/solution.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+base = int(input())
+power = int(input())
+modules = int(input())
+
+print(pow(base, power))
+print(pow(base, power, modules))
