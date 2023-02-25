c68dab6fb0dde6fdd677dcdd6985d1b8f8efcd81
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-02-25 22:27:35 +0300

    Time: 35 ms (76.56%), Space: 14.2 MB (25.54%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,7 +1,9 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        winner = 0
-        for i in range(1,n+1):
-            winner = (winner + k) % i
-            
-        return winner + 1
+        #base case: if there is one play left, return the number of the play
+        if n == 1:
+            return 1
+        # recursively call the function with n-1 players and k step size
+        # add k-1 to the result to account for the eliminated player
+        # use modulo to wrap around to the beginning of the circle
+        return (self.findTheWinner(n - 1, k) + k - 1)  % n + 1
