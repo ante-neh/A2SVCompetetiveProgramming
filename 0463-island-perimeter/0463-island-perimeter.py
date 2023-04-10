8c922f15776dea3d283726afc6c65893774abc90
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-04-10 14:39:23 +0300

    Time: 646 ms (26.69%), Space: 28.3 MB (11.66%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,24 +1,26 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        perimeter = 0
-        for r in range(rows):
-            for c in range(cols):
-                count = 0
+        visited = set()        
+        def dfs(row, col):
+            if row >= len(grid) or col >= len(grid[0]) or col < 0 or row < 0 or grid[row][col] == 0:
+                return 1
+            
+            if (row, col) in visited:
+                return 0
+            visited.add((row, col))
+            perimeter = dfs(row, col + 1)
+            perimeter += dfs(row, col - 1)
+            perimeter += dfs(row + 1, col)
+            perimeter += dfs(row - 1, col)
+            
+            return perimeter
+        
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
                 if grid[r][c] == 1:
-                    if r - 1 >= 0 and grid[r - 1][c] == 1:
-                        count += 1
-                    if r + 1 < rows and grid[r + 1][c] == 1:
-                        count += 1
-
-                    if c - 1 >= 0 and grid[r][c - 1] == 1:
-                        count += 1
-
-                    if c + 1 < cols and grid[r][c + 1] == 1:
-                        count += 1
-                    perimeter += (4 - count)
+                    return dfs(r, c)
                     
-        return perimeter
+        
                     
                 
                 
