df635201deeb541cce63e60ae18800b23f30cc35
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-04-25 19:22:43 +0300

    Time: 704 ms (61.91%), Space: 22 MB (32.26%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,33 +1,35 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        group = [-1 for _ in range(n)]
-
-        def dfs(index):
-            if group[index-1] == -1:
-                group[index-1] = 0
-
-            for neighbor in graph[index]:
-                if group[neighbor-1] == -1:
-                    group[neighbor-1] = 1 - group[index-1]
-                    if not dfs(neighbor):
+        for v, u in dislikes:
+            graph[v].append(u)
+            graph[u].append(v)
+            
+        visited = [0 for _ in range(n + 1)]
+        color = [0 for _ in range(n + 1)]
+        
+        def dfs(visited, color, node):
+            for neighbor in graph[node]:
+                if visited[neighbor] == False:
+                    visited[neighbor] = True
+                    color[neighbor] = not color[node]
+                    
+                    if not dfs(visited, color, neighbor):
                         return False
-
-                elif group[neighbor-1] == group[index-1]:
+                    
+                elif color[node] == color[neighbor]:
                     return False
-
+                
             return True
-
-        for src, des in dislikes:
-            graph[src].append(des)
-            graph[des].append(src)
-
+                
         for i in range(1, n + 1):
-            if group[i-1] == -1:
-                if not dfs(i):
+            if visited[i] == False:
+                if not dfs(visited, color,i):
                     return False
-
+                
         return True
+        
+            
             
         
             
