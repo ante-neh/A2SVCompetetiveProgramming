3bf2c622625d2ebf1e5d73eb238152a42b30b791
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-05-03 14:53:15 +0300

    Time: 71 ms (8.46%), Space: 16.3 MB (7.65%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,35 +3,33 @@ class Solution:
         queue = deque()
         fresh = 0
         minNumberOfMinutes = 0
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        visited = set()
         
         for r in range(len(grid)):
-            for c in range(len(grid[r])):
+            for c in range(len(grid[0])):
                 if grid[r][c] == 2:
-                    queue.append((r,c))
+                    queue.append((r, c))
+                    visited.add((r, c))
                 if grid[r][c] == 1:
                     fresh += 1
                     
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
         while queue and fresh:
-            for i in range(len(queue)):
+            for _ in range(len(queue)):
                 row, col = queue.popleft()
-                for r, c in directions:
-                    newRow, newCol = row + r, col + c
-                    if (newRow < 0 or newRow >= len(grid) or 
+                for rowChange, colChange in directions:
+                    newRow, newCol = row + rowChange, col + colChange
+                    if (newRow < 0 or newRow >= len(grid) or
                         newCol < 0 or newCol >= len(grid[0]) or 
-                        grid[newRow][newCol] == 0 or grid[newRow][newCol] == 2):
+                        (newRow, newCol) in visited or grid[newRow][newCol] == 0):
                         continue
-                    
+
                     grid[newRow][newCol] = 2
                     queue.append((newRow, newCol))
+                    visited.add((newRow, newCol))
                     fresh -= 1
-                    
+            
             minNumberOfMinutes += 1
-                
+            
         return minNumberOfMinutes if fresh == 0 else -1
-                    
-                    
-                    
-        
-        
+                
