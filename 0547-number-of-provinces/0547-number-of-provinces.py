f6d168abb947a18a5a5fcd4891ef7e666703181a
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-05-16 23:39:16 +0300

    Time: 228 ms (13.71%), Space: 16.7 MB (29.97%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,27 +1,52 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        count = 0
-        visited = set()
+        connections = {i:i for i in range(len(isConnected))}
+        size = [1] * len(isConnected)
         
         for r in range(len(isConnected)):
-            for c in range(len(isConnected)):
+            for c in range(len(isConnected[r])):
                 if isConnected[r][c] == 1:
-                    graph[r + 1].append(c + 1)
-                    graph[c + 1].append(r + 1)
-                    
-        def dfs(node):
-            visited.add(node)
-            for neighbour in graph[node]:
-                if not neighbour in visited:
-                    dfs(neighbour)
-                    
+                    if self.find(connections, r) != self.find(connections, c):
+                        self.union(connections, size, r, c)
         
-        for i in graph:
-            if not i in visited:
-                count += 1
-                dfs(i)
+        return self.count(connections)
+        
+    def find(self, connections, x):
+        parent = connections[x]
+        while parent != connections[parent]:
+            parent = connections[parent]
+        
+        while x != connections[x]:
+            connections[x] = connections[parent]
+            x = connections[x]
+        
+        return x
+    
+    def union(self, connections, size, x, y):
+        xrep, yrep = self.find(connections, x), self.find(connections, y)
+        if xrep != yrep:
+            if size[xrep] > size[yrep]:
+                connections[yrep] = xrep
+                size[xrep] += size[yrep]
+                self.update_connections(connections, yrep, xrep)
+            else:
+                connections[xrep] = yrep
+                size[yrep] += size[xrep]
+                self.update_connections(connections, xrep, yrep)
+
+    def update_connections(self, connections, old_rep, new_rep):
+        for node, parent in connections.items():
+            if parent == old_rep:
+                connections[node] = new_rep
                 
-        return count
+    def count(self, connections):
+        count = set()
+        for item in connections.items():
+            count.add(item[1])
+            
+        return len(count)
+                
+        
+                    
                     
         
