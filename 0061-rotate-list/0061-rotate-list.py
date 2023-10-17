4bba4c39a7396a0d854f8d514e9e2972e9070892
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-10-17 22:49:20 +0300

    Time: 42 ms (66.31%), Space: 16.3 MB (30.19%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -5,34 +5,51 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        nums = []
-        dummy = ListNode()
-        cur = dummy
-        if not head:
-            return None
+        if not head or not head.next:
+            return head
         
-        while head:
-            nums.append(head.val)
-            head = head.next
-            
-        nums = nums[::-1]
-        k = k % len(nums)
-        l, r = 0, k - 1
+        current = head
+        prev = None
+        n = 0
+        temp = head
+        while temp:
+            temp = temp.next
+            n += 1
         
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
+        k = k % n
+        if k == 0:
+            return head
             
-        l, r = k, len(nums) - 1
-        
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
+        while current:
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+            n += 1
+
+        leftStart = prev
+        current = prev
+        prev = None
+
+
+        while k > 0 and current:
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+            k -= 1
+
+        newHead = prev
+        prev = None 
+
+        while current:
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
             
-        for num in nums:
-            cur.next = ListNode(num)
-            cur = cur.next
-            
-        return dummy.next
+        leftStart.next = prev
+
+        return newHead
+
+
