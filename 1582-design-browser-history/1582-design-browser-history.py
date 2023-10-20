457c872f70e12d55f79a90a52bbc3128eaf4ebdf
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-10-20 21:01:53 +0300

    Time: 256 ms (32.23%), Space: 19.2 MB (13.21%) - LeetHub

--- 1582-design-browser-history/1582-design-browser-history.py
+++ 1582-design-browser-history/1582-design-browser-history.py
@@ -1,31 +1,54 @@
 class Node:
-    def __init__(self, val = 0, prev = None, next = None):
+    def __init__(self, val = 0, next = None):
         self.val = val
-        self.prev = prev
         self.next = next
 
 class BrowserHistory:
 
     def __init__(self, homepage: str):
         self.head = Node(homepage)
+        self.cur = self.head
+        self.curLength = 1
+        self.length = 1
 
     def visit(self, url: str) -> None:
-        self.head.next = Node(url, self.head)
-        self.head = self.head.next
+        self.cur.next = Node(url)
+        self.cur = self.cur.next
+        self.curLength += 1
+        self.length = self.curLength
 
     def back(self, steps: int) -> str:
-        while self.head.prev and steps > 0:
-            self.head = self.head.prev
-            steps -= 1
+        if self.curLength - steps < 0:
+            self.cur = self.head
+            self.curLength = 0
+            return self.cur.val
+        
+        else:
+            slow, fast = self.head, self.head
+            while fast and steps > 0:
+                fast = fast.next
+                steps -= 1
+                self.curLength -= 1
+            
+            while fast and fast.val != self.cur.val:
+                slow = slow.next
+                fast = fast.next
 
-        return self.head.val
+            self.cur = slow
+
+            return self.cur.val
 
     def forward(self, steps: int) -> str:
-        while self.head.next and steps > 0:
-            self.head = self.head.next
+        temp = self.cur
+        self.curLength = min(self.curLength + steps, self.length)
+
+        while temp.next and steps > 0:
+            temp = temp.next
             steps -= 1
 
-        return self.head.val
+        self.cur = temp
+
+        return self.cur.val
 
 
 # Your BrowserHistory object will be instantiated and called as such:
