5537f9adccdaa641cbfeb0b67e6c82b1c0fa0c77
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-10-22 23:43:51 +0300

    Time: 710 ms (40.17%), Space: 78.5 MB (18.48%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,100 +1,43 @@
-class Node(object):
+class Node:
     def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.prev = None
-        self.next = None
-        
-class LinkedList(object):
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        
-    def moveToHead(self, node):
-
-        if self.head == self.tail or self.head == node:
-            return
-
-        if node.next is None:
-            self.tail = node.prev
-        else:
-            node.next.prev = node.prev
-        
-        node.prev.next = node.next
-        node.next = self.head
-        self.head.prev = node
-        self.head = node
-    
-    def addNode(self, key, val):
-        node = Node(key, val)
-        if self.head is None:
-            self.head = node
-            self.tail = node
-        else:
-            node.next = self.head
-            self.head.prev = node
-            self.head = node
-        return node
-  
-    def removeTail(self):
-        
-        if self.head == self.tail:
-            self.head = None
-            self.tail = None
-            return
-        
-        self.tail = self.tail.prev
-        self.tail.next = None
-        
-class LRUCache(object):
-
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
-        self.capacity = capacity
-        self.linkedList = LinkedList()
-        self.lruMap = {}
-
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-            
-        if key not in self.lruMap:
-            return -1
-        
-        node = self.lruMap.get(key)
-        val = node.val
-        self.linkedList.moveToHead(node)
-
-        return val
-
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: None
-        """
-        
-        if key in self.lruMap:
-            node = self.lruMap[key]
-            node.val = value
-            self.linkedList.moveToHead(node)
-            return
-            
-        if self.capacity == len(self.lruMap):
-            del self.lruMap[self.linkedList.tail.key]
-            self.linkedList.removeTail()
-            
-        node = self.linkedList.addNode(key, value)
-        self.lruMap[key] = node
-        
-        
+        self.key, self.val = key, val
+        self.prev = self.next = None
 
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}  # map key to node
+
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    # remove node from list
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    # insert node at right
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            # remove from the list and delete the LRU from hashmap
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
