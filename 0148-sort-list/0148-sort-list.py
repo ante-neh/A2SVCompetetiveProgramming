1dd7429414d6d4bc9b3459817cfad1f58fb1118b
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-10-22 09:41:30 +0300

    Time: 552 ms (22.36%), Space: 38.6 MB (69.61%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,41 +5,63 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
+        if not head:
+            return None
+        
+        if head and not head.next:
             return head
-        
-        fast, slow = head.next, head
+
+        slow, fast = head, head.next
+
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
-            
-        mid = slow.next
+            slow = slow.next
+
+        right = slow.next
         slow.next = None
+
+        leftHalf = self.sortList(head)
+        rightHalf = self.sortList(right)
         
-        left = self.sortList(head)
-        right = self.sortList(mid)
-        
-        return self.merge(left, right)
-    
+        return self.merge(leftHalf, rightHalf)
+
     def merge(self, list1, list2):
+        p1, p2 = list1, list2
         dummy = ListNode()
-        cur = dummy 
-        while list1 and list2:
-            if list1.val < list2.val:
-                cur.next = list1
-                list1 = list1.next
-                
+        cur = dummy
+        if not p1:
+            return p2
+
+        if not p2:
+            return p1
+
+        if not p1 and not p2:
+            return None
+
+        while p1 and p2:
+            if p1.val <= p2.val:
+                cur.next = p1
+                cur = cur.next
+                p1 = p1.next
 
             else:
-                cur.next = list2
-                list2 = list2.next
-            
+                cur.next = p2
+                cur = cur.next
+                p2 = p2.next
+
+        while p1:
+            cur.next = p1
             cur = cur.next
-            
-        if list1:
-            cur.next = list1
-        if list2:
-            cur.next = list2
-            
-        return dummy.next
+            p1 = p1.next
+
+        while p2:
+            cur.next = p2
+            cur = cur.next
+            p2 = p2.next
+
         
+        return dummy.next
+
+
+
+
