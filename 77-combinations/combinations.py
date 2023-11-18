73388f0062f69542a5a73d8d9f861cb8147b73fe
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-11-18 14:09:39 +0300

    Time: 944 ms (29.79%) | Memory: 64.9 MB (31.20%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,16 +1,16 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         combinations = []
-        def dfs(i, cur):
-            if len(cur) >= k:
+        def dfs(cur, index):
+            if len(cur) == k:
                 combinations.append(cur[:])
                 return
 
-            for i in range(i, n + 1):
+            for i in range(index, n + 1):
                 cur.append(i)
-                dfs(i + 1, cur)
+                dfs(cur, i + 1)
                 cur.pop()
-        
-        dfs(1, [])
+
+        dfs([], 1)
 
         return combinations
