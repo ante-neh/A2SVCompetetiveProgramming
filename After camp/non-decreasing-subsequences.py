a18ddc86069d95ee13b91ed3b779408d2ff284e2
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-11-20 21:58:52 +0300

    Added solution for Non-decreasing Subsequences

---
+++ After camp/non-decreasing-subsequences.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        subSequences = set()
+
+        def backtrack(cur, index):
+            if index > len(nums):
+                return
+
+            if len(cur) >= 2:
+                subSequences.add(tuple(cur[:]))
+
+            for i in range(index, len(nums)):
+                if not cur:
+                    cur.append(nums[i])
+                    backtrack(cur, i + 1)
+                    cur.pop()
+                                       
+                if (cur and cur[-1] <= nums[i]):
+                    cur.append(nums[i])
+                    backtrack(cur, i + 1)
+                    cur.pop()
+
+                
+
+        backtrack([], 0)
+        
+        return subSequences
+
