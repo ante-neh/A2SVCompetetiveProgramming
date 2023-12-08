2547a333cd70078351c39cce427209a544e97fdb
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-12-08 22:20:10 +0300

    Time: 262 ms (48.54%) | Memory: 25.1 MB (22.98%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.py
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
-        nums = []
+        nums1, nums2 = [], []
         def dfs(node, result):
             if not node:
                 return
@@ -17,8 +17,22 @@ class Solution:
 
             return result
 
-        nums.extend(dfs(root1, [])) if root1 else []
-        nums.extend(dfs(root2, [])) if root2 else []
-        nums.sort()
+        nums1.extend(dfs(root1, [])) if root1 else []
+        nums2.extend(dfs(root2, [])) if root2 else []
 
-        return nums 
+        p1, p2, result = 0, 0, []
+        
+        while p1 < len(nums1) and p2 < len(nums2):
+            if nums1[p1] < nums2[p2]:
+                result.append(nums1[p1])
+                p1 += 1
+            else:
+                result.append(nums2[p2])
+                p2 += 1
+
+        if p1 < len(nums1):
+            result.extend(nums1[p1:])
+        if p2 < len(nums2):
+            result.extend(nums2[p2:])
+
+        return result
