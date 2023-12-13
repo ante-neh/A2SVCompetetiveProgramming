34e27b235648d2b9b77cc32fda38afffe0b8cc8e
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-12-13 14:43:51 +0300

    Added solution for Count Complete Tree Nodes

---
+++ After camp/count-complete-tree-nodes.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        queue = deque([root])
+        count = 0
+        
+        if not root:
+            return count 
+
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            count += len(level)
+
+        return count 
