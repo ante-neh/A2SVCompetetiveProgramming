fa48ea9000e4d25bd621fb3abc2ced1c255f476a
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-12-17 11:31:32 +0300

    Time: 39 ms (50.12%) | Memory: 16.4 MB (7.92%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -9,10 +9,12 @@ class Solution:
         def dfs(node1, node2):
             if not node1 and not node2:
                 return True
-            
             if not node1 or not node2:
                 return False
+            
+            if node1.val != node2.val:
+                return False
 
-            return node1.val == node2.val and dfs(node1.left, node2.left) and dfs(node1.right, node2.right)
+            return dfs(node1.left, node2.left) and dfs(node1.right, node2.right)
 
         return dfs(p, q)
