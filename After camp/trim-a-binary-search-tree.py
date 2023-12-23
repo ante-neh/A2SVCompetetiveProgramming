68d2370868a0864f15b2ae38e70ba2434470c338
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2023-12-23 10:34:46 +0300

    Added solution for Trim a Binary Search Tree

--- After camp/trim-a-binary-search-tree.py
+++ After camp/trim-a-binary-search-tree.py
@@ -8,16 +8,17 @@ class Solution:
     def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
         def dfs(node):
             if not node:
-                return
+                return 
 
             if node.val < low:
                 return dfs(node.right)
-            
-            if node.val > high:
+            elif node.val > high:
                 return dfs(node.left)
+            else:
+                node.left = dfs(node.left)
+                node.right = dfs(node.right)
 
-            node.left = dfs(node.left)
-            node.right = dfs(node.right)
-            
             return node
+
         return dfs(root)
+
