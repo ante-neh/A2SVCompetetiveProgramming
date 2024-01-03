fc8dc6bfc102f2f7af2f16044f37c4ddaa1d66ea
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2024-01-03 15:52:47 +0300

    Added solution for Missing Number

--- After camp/missing-number.py
+++ After camp/missing-number.py
@@ -1,3 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        return sum(range(0, len(nums) + 1)) - sum(nums)
+        i = 0
+
+        while i < len(nums):
+            j = nums[i]
+            
+            if j < len(nums) and nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+
+        return len(nums)
