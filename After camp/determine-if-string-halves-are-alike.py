80dadc7382fcf7645393c12aa6c43ba2c53e3a36
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2024-01-12 06:33:54 +0300

    Added solution for Determine if String Halves Are Alike

--- After camp/determine-if-string-halves-are-alike.py
+++ After camp/determine-if-string-halves-are-alike.py
@@ -2,15 +2,15 @@ class Solution:
     def halvesAreAlike(self, s: str) -> bool:
         vowels = set(['a','e','i','o','u', 'A', 'E', 'I','O','U'])
         leftCount, rightCount = 0, 0
-        half = len(s) // 2
+        left, right = 0, len(s) - 1
 
-        for i in range(len(s)):
-            if i < half:
-                if s[i] in vowels:
-                    leftCount += 1
-            
-            else:
-                if s[i] in vowels:
-                    rightCount += 1
+        while left < right:
+            if s[left] in vowels:
+                leftCount += 1
+            if s[right] in vowels:
+                rightCount += 1
+
+            left += 1
+            right -= 1
 
         return leftCount == rightCount
