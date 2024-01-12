29e1c98e6d6bbf9414966bbd76b42c2689357439
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2024-01-12 17:36:23 +0300

    Time: 31 ms (90.96%) | Memory: 17.2 MB (25.91%) - LeetSync

--- 461-hamming-distance/hamming-distance.py
+++ 461-hamming-distance/hamming-distance.py
@@ -1,12 +1,12 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
+        xor = x ^ y
         distance = 0
 
-        while x != y:
-            if x & 1 != y & 1:
+        while xor:
+            if xor & 1:
                 distance += 1
-
-            x >>= 1
-            y >>= 1
+            
+            xor >>= 1
 
         return distance
