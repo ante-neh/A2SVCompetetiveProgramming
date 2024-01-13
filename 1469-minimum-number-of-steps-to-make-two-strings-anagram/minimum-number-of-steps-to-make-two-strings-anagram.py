950df70e90500db9b3debc0cad3b6b8f98f24860
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2024-01-13 15:21:46 +0300

    Time: 105 ms (85.45%) | Memory: 17.7 MB (24.39%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,12 +1,10 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
         sCount, tCount = Counter(s), Counter(t)
+        operations = 0
 
-        def count(str1, str2, count1, count2):
-            counter = 0
-            for c in str1:
-                counter += count1[c] - count2[c] if count1[c] >= count2[c] else 0
+        for value, freq in sCount.items():
+            if freq - tCount[value] > 0:
+                operations += freq - tCount[value]
 
-            return counter
-
-        return min(count(list(set(s)), list(set(t)), sCount, tCount), count(list(set(t)), list(set(s)), tCount, sCount))
+        return operations
