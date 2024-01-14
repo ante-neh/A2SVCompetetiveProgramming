e91b6ea0562313282bbf985d5f76a9350a5e18ae
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2024-01-14 11:52:09 +0300

    Time: 116 ms (87.03%) | Memory: 20.1 MB (71.20%) - LeetSync

---
+++ 1254-deepest-leaves-sum/deepest-leaves-sum.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, depth):
+            if not node:
+                return [float("-inf"), float("-inf")]
+            
+            if not node.left and not node.right:
+                return [node.val, depth]
+
+            left = dfs(node.left, depth + 1)
+            right = dfs(node.right, depth + 1)
+
+            if left[1] == right[1]:
+                return [right[0] + left[0], left[1]]
+            
+            elif left[1] > right[1]:
+                return left
+
+            return right
+
+        result = dfs(root, 0)
+
+        return result[0]
