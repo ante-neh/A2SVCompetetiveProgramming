dd8b4b00222d97a8daef0036e3087bda47ec2738
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2024-01-24 00:12:13 +0300

    Time: 65 ms (89.19%) | Memory: 54.2 MB (5.07%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.ts
+++ 10-regular-expression-matching/regular-expression-matching.ts
@@ -1,60 +1,55 @@
 function isMatch(s: string, p: string): boolean {
-  const memo: boolean[][] = [];
+  const memo:boolean[][] = []
 
-  // this is the main DP recursion. its the exact same as it would work recursively
-  // except that we cache the results before returning it
-  function isMatchDp(i:number,j:number): boolean {
-    // if it exists in cache then return it
-    if (getMemo(i,j) !== undefined) {
-        return getMemo(i,j);
-    }
-    // BASE CASE:
-    // if theres remaining values in string, but none in pattern then we didn't match everything return false
-    if (i < s.length && j === p.length) return setMemo(i,j,false);
-    // if theres nothing left to match then we've matched everything, return true
-    if (i === s.length && j === p.length) return setMemo(i,j,true);
-
-    // STAR CASE:
-    if (p[j+1] === "*") {
-      if (matchFirstChar(i, j)) {
-        // if the chars match then either:
-        // take one character and keep star
-        // do nothing and remove star
-        // if either recurses true, then we got at least one match, so the whole expression is valid
-        return setMemo(i,j,isMatchDp(i+1, j) || isMatchDp(i, j+2));
+  function isMatchDp(i:number, j:number):boolean{
+      if(getMemo(i, j) !== undefined){
+          return getMemo(i, j)
       }
-      // otherwise if nothing matches, then we remove star and continue
-      return setMemo(i,j,isMatchDp(i, j+2));
+
+      if(i < s.length && j === p.length){ 
+          return setMemo(i, j, false)
+          }
+
+      if(i === s.length && j === p.length) {
+          return setMemo(i, j, true)
+        }
+
+    if(p[j + 1] === "*"){
+        if(matchFirstChar(i, j)){
+            return setMemo(i, j, isMatchDp(i + 1, j) || isMatchDp(i, j + 2))
+        }
+
+        return setMemo(i, j, isMatchDp(i, j + 2))
     }
 
-    // REGULAR CASE:
-    if (matchFirstChar(i,j)) {
-      // successful match, recurse
-      return setMemo(i,j,isMatchDp(i+1, j+1));
+    if(matchFirstChar(i, j)){
+        return setMemo(i, j, isMatchDp(i + 1,  j + 1))
     }
 
-    // first characters never matched, return false
-    return setMemo(i,j,false);
+
+    return setMemo(i, j, false)
+
   }
 
-  function getMemo(i:number, j:number): boolean {
-    return memo?.[i]?.[j];
+  function getMemo(i:number, j:number):boolean{
+      return memo?.[i]?.[j]
   }
-  function setMemo(i:number, j:number, val:boolean): boolean {
-      if (memo[i] === undefined) {
-        memo[i] = [];
+
+
+  function setMemo(i:number, j:number, val:boolean):boolean{
+      if(memo[i] === undefined){
+          memo[i] = []
       }
-      memo[i][j] = val;
-      return val;
+      memo[i][j] = val
+      return val
   }
 
-  // if chars match either because they are the same or pattern is "."
-  function matchFirstChar(i:number, j:number): boolean {
-    if (s[i] === undefined) return false;
-    if (p[j] === ".") return true;
-    return s[i] === p[j];
+  function matchFirstChar(i:number, j:number):boolean{
+      if(s[i] === undefined) return false
+      if(p[j] === ".") return true
+
+      return s[i] === p[j]
   }
 
-
-  return isMatchDp(0,0);
+  return isMatchDp(0, 0)
 }
