6b1f15de2bffeec1f34cba9bd559708a3344289a
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2024-01-24 23:28:19 +0300

    Added solution for Pseudo-Palindromic Paths in a Binary Tree

--- After camp/pseudo-palindromic-paths-in-a-binary-tree.py
+++ After camp/pseudo-palindromic-paths-in-a-binary-tree.py
@@ -6,29 +6,28 @@
 #         self.right = right
 class Solution:
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
-        self.count = 0
-        def dfs(node, cur):
+        count = 0
+        def dfs(node, nodeFreq):
+            nonlocal count
             if not node:
                 return
 
-            cur[node.val] += 1
+            nodeFreq[node.val] += 1
             if not node.left and not node.right:
-                countPartiy = 0
-                for val in cur.values():
-                    if val % 2 == 1:
-                        countPartiy += 1
-                        
-                if countPartiy < 2:
-                    self.count += 1
+                c = 0
+                for item in nodeFreq.items():
+                    if item[1] % 2 != 0:
+                        c += 1
 
-                cur[node.val] -= 1
-                return 
+                if c < 2:
+                    count += 1
+                nodeFreq[node.val] -= 1
+                return
 
-            dfs(node.left, cur)
-            dfs(node.right, cur)
-            cur[node.val] -= 1
+            dfs(node.left, nodeFreq)
+            dfs(node.right, nodeFreq)
+            nodeFreq[node.val] -= 1
 
         dfs(root, defaultdict(int))
 
-        return self.count
-
+        return count
