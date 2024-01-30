16f828212f7858e07d2f4ab8351bc3f3c9245827
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2024-01-30 10:07:13 +0300

    Added solution for Evaluate Reverse Polish Notation

--- After camp/evaluate-reverse-polish-notation.py
+++ After camp/evaluate-reverse-polish-notation.py
@@ -3,24 +3,25 @@ class Solution:
         stack = []
 
         for token in tokens:
-            if token == "+":
-                num1, num2 = stack.pop(), stack.pop()
-                stack.append(num1 + num2)
-            
+            if token == '+':
+                stack.append(stack.pop() + stack.pop())
             elif token == '-':
-                num1, num2 = stack.pop(), stack.pop()
-                stack.append(num2 - num1)
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(val2 - val1)
+
             elif token == '*':
-                num1, num2 = stack.pop(), stack.pop()
-                stack.append(num1 * num2)
+                stack.append(stack.pop() * stack.pop())
             
-            elif token == "/":
-                num1 = stack.pop()
-                num2 = stack.pop()
-                stack.append(int(num2 / num1))
+            elif token == '/':
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(int(val2 / val1))
 
             else:
                 stack.append(int(token))
-
+            
 
         return stack[-1]
+
+            
