a2c5468a169d3655c2e18f4d475602e166cdecfc
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2024-02-19 18:56:01 +0300

    Added solution for Find Greatest Common Divisor of Array

--- After camp/find-greatest-common-divisor-of-array.py
+++ After camp/find-greatest-common-divisor-of-array.py
@@ -2,10 +2,9 @@ class Solution:
     def findGCD(self, nums: List[int]) -> int:
         a, b = max(nums), min(nums)
 
-        def gcd(a, b):
-            if b == 0:
-                return a
-            
-            return gcd(b, a % b)
+        while b > 0:
+            temp = a % b
+            a = b 
+            b = temp
 
-        return gcd(a, b)
+        return a
