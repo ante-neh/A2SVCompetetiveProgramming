fc633ddccd4da220e85d79dfcbb2ec9853378164
Anteneh Amare <79926248+ante-neh@users.noreply.github.com>
2024-03-02 14:27:27 +0300

    Added solution for Smallest String With Swaps

--- After camp/smallest-string-with-swaps.py
+++ After camp/smallest-string-with-swaps.py
@@ -1,40 +1,41 @@
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
-        n = len(s)
-        self.root = [i for i in range(n)]
-        self.rank = [1] * n
-        graph = defaultdict(list)
-        lexicographicallySmallest = []
+        self.root = [i for i in range(len(s))]
+        self.rank = [1] * len(s)
+        result = []
+
+        rootToChar = defaultdict(list)
 
         for i, j in pairs:
             self.union(i, j)
+            
+        for i in range(len(s)):
+            rootToChar[self.find(i)].append(s[i])
 
-        for i in range(n):
-            graph[self.find(i)].append(s[i])
+        for item in rootToChar.items():
+            item[1].sort(reverse = True)
 
-        for item in graph.items():
-            item[1].sort()
+        for i in range(len(s)):
+            result.append(rootToChar[self.find(i)].pop())
 
-        for i in range(n):
-            lexicographicallySmallest.append(graph[self.find(i)].pop(0))
-        
-        return "".join(lexicographicallySmallest)
+        return "".join(result)
 
     def find(self, x):
-        while x != self.root[x]:
-            x = self.root[x]
+        if x == self.root[x]:
+            return x
 
-        return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
 
     def union(self, x, y):
-        xrep, yrep = self.find(x), self.find(y)
-        if xrep != yrep:
-            if self.rank[xrep] > self.rank[yrep]:
-                self.root[yrep] = xrep
-            elif self.rank[yrep] > self.rank[xrep]:
-                self.root[xrep] = yrep
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX != rootY:
+            if self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+                self.rank[rootY] = self.rank[rootX]
+
             else:
-                self.root[yrep] = xrep
-                self.rank[xrep] += 1
-                
-            
+                self.root[rootY] = rootX
+                self.rank[rootX] = self.rank[rootY] 
